37114c649749b2af327b761ab552f009487cf4ee
ad-ppe <adriendebreuilly.ppe@gmail.com>
2025-05-06 15:03:00 +0200

    fuck

--- core/example.py
+++ core/example.py
@@ -1,7 +1,8 @@
 # example.py
 
 def hello_world():
-    print("Bonjour à tous depuis une feature !")
+    print("Bonjour à tous depuis une feature ! fuck")
+    print("mpedhfucc oicbnzecoienczedoibndodsin845120")
 
 if __name__ == "__main__":
     hello_world()
